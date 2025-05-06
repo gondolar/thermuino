4a6ef1531d77e7b5a41701cd5cab915c9ad58d42
The Dragon <pabloarielz@outlook.com>
2025-05-06 05:44:38 -0300

    + Minor additon for debugging.

--- src/tno_main.cpp
+++ src/tno_main.cpp
@@ -1,6 +1,9 @@
 #include "tno_main.h"
+#include "llc_arduino.h"
 
 #include <LittleFS.h>
+
+#include <user_interface.h>
 
 LLC_USING_TYPEINT();
 
@@ -13,9 +16,10 @@ void setup() {
     DallasTemperature   & ds18b20   = *g_App.ApiDS18B20;
     ds18b20.begin();
     if_zero_e(LittleFS.begin());
+    uint8 currentFrequency = system_get_cpu_freq();  // returns 80 or 160
+    info_printf("System frequency: %u MHz.", currentFrequency);
 
     info_printf("Scanning for DS18B20 sensors...");
-    //DeviceAddress     deviceAddress     = {};
     uint8_t         deviceAddress[8]  = {};
     while (g_App.ApiOneWire->search(deviceAddress)) {
         if_fail_e(g_App.DS18B20Addresses.push_back(*(u3_t*)deviceAddress));
@@ -29,7 +33,7 @@ void loop() {
     DallasTemperature   & ds18b20   = *g_App.ApiDS18B20;
     ds18b20.requestTemperatures();
     for(u2_t i=0; i < g_App.DS18B20Addresses.size(); ++i) {
-        cnst    float   tempC   = ds18b20.getTempCByIndex(i);  // 
-        info_printf("DS18B20 measured temperatura: %f.", tempC);
+        cnst    float   tempC   = ds18b20.getTempC((const uint8_t*)&g_App.DS18B20Addresses[i]);  // 
+        info_printf("DS18B20 measured temperature: %f.", tempC);
     }
 }
