e753ffb98b0146d7d5fe3a152503d01ad869c890
The Dragon <pabloarielz@outlook.com>
2025-06-07 03:27:41 -0300

    - Added missing configuration for the IR receiver

--- src/tno_main.cpp
+++ src/tno_main.cpp
@@ -8,7 +8,15 @@
 
 #include <LittleFS.h>
 
-#define IR_RECEIVE_PIN 6 
+
+using tno::WEMOS_PIN_MAP_DIGITAL;
+
+#define FEEDBACK_LED_IS_ACTIVE_LOW // The LED on my board (D4) is active LOW
+#define IR_RECEIVE_PIN          14 // D5
+#define IR_SEND_PIN             12 // D6 - D4/pin 2 is internal LED
+#define _IR_TIMING_TEST_PIN      2 // D4
+#define APPLICATION_PIN         13 // D7
+
 #include "TinyIRReceiver.hpp" // include the code
 
 
@@ -17,9 +25,249 @@ LLC_USING_TYPEINT();
 SPIClass SPI2(HSPI);
 SPIClass SPI3(VSPI);
 
+void testlines(Adafruit_ST7735 & tft, uint16_t color) {
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(0, 0, x, tft.height()-1, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(0, 0, tft.width()-1, y, color);
+    delay(0);
+  }
+
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(tft.width()-1, 0, 0, y, color);
+    delay(0);
+  }
+
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(0, tft.height()-1, x, 0, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
+    delay(0);
+  }
+
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
+    delay(0);
+  }
+}
+
+void testdrawtext(Adafruit_ST7735 & tft, char *text, uint16_t color) {
+  tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
+}
+
+void testfastlines(Adafruit_ST7735 & tft, uint16_t color1, uint16_t color2) {
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t y=0; y < tft.height(); y+=5) {
+    tft.drawFastHLine(0, y, tft.width(), color1);
+  }
+  for (int16_t x=0; x < tft.width(); x+=5) {
+    tft.drawFastVLine(x, 0, tft.height(), color2);
+  }
+}
+
+void testdrawrects(Adafruit_ST7735 & tft, uint16_t color) {
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color);
+  }
+}
+
+void testfillrects(Adafruit_ST7735 & tft, uint16_t color1, uint16_t color2) {
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=tft.width()-1; x > 6; x-=6) {
+    tft.fillRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color1);
+    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color2);
+  }
+}
+
+void testfillcircles(Adafruit_ST7735 & tft, uint8_t radius, uint16_t color) {
+  for (int16_t x=radius; x < tft.width(); x+=radius*2) {
+    for (int16_t y=radius; y < tft.height(); y+=radius*2) {
+      tft.fillCircle(x, y, radius, color);
+    }
+  }
+}
+
+void testdrawcircles(Adafruit_ST7735 & tft, uint8_t radius, uint16_t color) {
+  for (int16_t x=0; x < tft.width()+radius; x+=radius*2) {
+    for (int16_t y=0; y < tft.height()+radius; y+=radius*2) {
+      tft.drawCircle(x, y, radius, color);
+    }
+  }
+}
+
+void testtriangles(Adafruit_ST7735 & tft) {
+  tft.fillScreen(ST77XX_BLACK);
+  uint16_t color = 0xF800;
+  int t;
+  int w = tft.width()/2;
+  int x = tft.height()-1;
+  int y = 0;
+  int z = tft.width();
+  for(t = 0 ; t <= 15; t++) {
+    tft.drawTriangle(w, y, y, x, z, x, color);
+    x-=4;
+    y+=4;
+    z-=4;
+    color+=100;
+  }
+}
+
+void testroundrects(Adafruit_ST7735 & tft) {
+  tft.fillScreen(ST77XX_BLACK);
+  uint16_t color = 100;
+  int i;
+  int t;
+  for(t = 0 ; t <= 4; t+=1) {
+    int x = 0;
+    int y = 0;
+    int w = tft.width()-2;
+    int h = tft.height()-2;
+    for(i = 0 ; i <= 16; i+=1) {
+      tft.drawRoundRect(x, y, w, h, 5, color);
+      x+=2;
+      y+=3;
+      w-=4;
+      h-=6;
+      color+=1100;
+    }
+    color+=100;
+  }
+}
+
+void tftPrintTest(Adafruit_ST7735 & tft) {
+  tft.setTextWrap(false);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 30);
+  tft.setTextColor(ST77XX_RED);
+  tft.setTextSize(1);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(3);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_BLUE);
+  tft.setTextSize(4);
+  tft.print(1234.567);
+  delay(1500);
+  tft.setCursor(0, 0);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(0);
+  tft.println("Hello World!");
+  tft.setTextSize(1);
+  tft.setTextColor(ST77XX_GREEN);
+  tft.print(p, 6);
+  tft.println(" Want pi?");
+  tft.println(" ");
+  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
+  tft.println(" Print HEX!");
+  tft.println(" ");
+  tft.setTextColor(ST77XX_WHITE);
+  tft.println("Sketch has been");
+  tft.println("running for: ");
+  tft.setTextColor(ST77XX_MAGENTA);
+  tft.print(millis() / 1000);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.print(" seconds.");
+}
+
+void mediabuttons(Adafruit_ST7735 & tft) {
+  // play
+  tft.fillScreen(ST77XX_BLACK);
+  tft.fillRoundRect(25, 10, 78, 60, 8, ST77XX_WHITE);
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_RED);
+  delay(500);
+  // pause
+  tft.fillRoundRect(25, 90, 78, 60, 8, ST77XX_WHITE);
+  tft.fillRoundRect(39, 98, 20, 45, 5, ST77XX_GREEN);
+  tft.fillRoundRect(69, 98, 20, 45, 5, ST77XX_GREEN);
+  delay(500);
+  // play color
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_BLUE);
+  delay(50);
+  // pause color
+  tft.fillRoundRect(39, 98, 20, 45, 5, ST77XX_RED);
+  tft.fillRoundRect(69, 98, 20, 45, 5, ST77XX_RED);
+  // play color
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_GREEN);
+}
+
+llc::err_t  testDisplayST7735(Adafruit_ST7735 & tft) {
+    uint16_t time = millis();
+    tft.fillScreen(ST77XX_BLACK);
+    time = millis() - time;
+
+    Serial.println(time, DEC);
+    delay(500);
+
+    // large block of text
+    tft.fillScreen(ST77XX_BLACK);
+    testdrawtext(tft, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST77XX_WHITE);
+    delay(1000);
+
+    // tft print function!
+    tftPrintTest(tft);
+    delay(4000);
+
+    // a single pixel
+    tft.drawPixel(tft.width()/2, tft.height()/2, ST77XX_GREEN);
+    delay(500);
+
+    // line draw test
+    testlines(tft, ST77XX_YELLOW);
+    delay(500);
+
+    // optimized lines
+    testfastlines(tft, ST77XX_RED, ST77XX_BLUE);
+    delay(500);
+
+    testdrawrects(tft, ST77XX_GREEN);
+    delay(500);
+
+    testfillrects(tft, ST77XX_YELLOW, ST77XX_MAGENTA);
+    delay(500);
+
+    tft.fillScreen(ST77XX_BLACK);
+    testfillcircles(tft, 10, ST77XX_BLUE);
+    testdrawcircles(tft, 10, ST77XX_WHITE);
+    delay(500);
+
+    testroundrects(tft);
+    delay(500);
+
+    testtriangles(tft);
+    delay(500);
+
+    mediabuttons(tft);
+    delay(500);
+    return 0;
+}
 llc::err_t  initDisplayST7735(tno::STempApp & app) {
     if_null_fe(app.ST7735.create(&SPI2, (s0_t)app.ST7735CS, (s0_t)app.ST7735DC, (s0_t)app.ST7735Reset));
-
+    app.ST7735->initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
+    ::testDisplayST7735(*app.ST7735);
     //display.display();  // Show initial display buffer contents on the screen -- the library initializes this with an Adafruit splash screen.
     return 0;
 }
@@ -96,17 +344,17 @@ void setup() {
     info_printf("setup() end.");
 }
 
-llc::err_t tickIRReceiver   (TinyIRReceiverCallbackDataStruct receiverData) {
-    if (false == TinyReceiverDecode(receiverData)) 
+llc::err_t tickIRReceiver   () {
+    if (false == TinyReceiverDecode()) 
         return 0;
-    info_printf("Flags:0x%x.", (u2_t)receiverData.Flags);
+    info_printf("Flags:0x%x.", (u2_t)TinyIRReceiverData.Flags);
 #ifndef USE_FAST_PROTOCOL
-    info_printf("Address:0x%x.", (u2_t)receiverData.Address); // We have no address at FAST protocol
+    info_printf("Address:0x%x.", (u2_t)TinyIRReceiverData.Address); // We have no address at FAST protocol
 #endif
-    info_printf("Command:0x%x.", (u2_t)receiverData.Command);
-    if_true_i(receiverData.Flags == IRDATA_FLAGS_IS_REPEAT);
-    if_true_fe(receiverData.Flags == IRDATA_FLAGS_PARITY_FAILED); //  Parity failed. Try defining USE_EXTENDED_NEC_PROTOCOL or USE_ONKYO_PROTOCOL.
-    return receiverData.Address | (receiverData.Command << (sizeof(receiverData.Address)*8));
+    info_printf("Command:0x%x.", (u2_t)TinyIRReceiverData.Command);
+    if_true_i(TinyIRReceiverData.Flags == IRDATA_FLAGS_IS_REPEAT);
+    if_true_fe(TinyIRReceiverData.Flags == IRDATA_FLAGS_PARITY_FAILED); //  Parity failed. Try defining USE_EXTENDED_NEC_PROTOCOL or USE_ONKYO_PROTOCOL.
+    return TinyIRReceiverData.Address | (TinyIRReceiverData.Command << (sizeof(TinyIRReceiverData.Address)*8));
 }
 
 void loop() { 
@@ -117,5 +365,5 @@ void loop() {
     //    f2_c tempC          = ds18b20.getTempC(deviceAddress);  // 
     //    info_printf("DS18B20 with id 0x%X%X measured temperature: %f.", *(const uint32_t*)(&deviceAddress[4]), *(const uint32_t*)(&deviceAddress[0]), tempC);
     //}
-    tickIRReceiver(TinyIRReceiverData);
+    tickIRReceiver();
 }
--- src/tno_version_info.h
+++ src/tno_version_info.h
@@ -6,16 +6,16 @@
 namespace tno
 {
     using ::llc::vcst_c;
-    stxp ::llc::u3_c    TNO_VERSION_TIME       = 1749270009;
+    stxp ::llc::u3_c    TNO_VERSION_TIME       = 1749276086;
     stxp ::llc::u1_c    TNO_VERSION_YEAR       = 2025;
     stxp ::llc::u0_c    TNO_VERSION_MONTH      = 6;
     stxp ::llc::u0_c    TNO_VERSION_MDAY       = 7;
-    stxp ::llc::u1_c    TNO_VERSION_HOUR       = 4;
-    stxp ::llc::u0_c    TNO_VERSION_MINUTE     = 20;
-    stxp ::llc::vcst_c  TNO_VERSION_STRING     = LLC_CXS("202506070420");
+    stxp ::llc::u1_c    TNO_VERSION_HOUR       = 6;
+    stxp ::llc::u0_c    TNO_VERSION_MINUTE     = 1;
+    stxp ::llc::vcst_c  TNO_VERSION_STRING     = LLC_CXS("202506070601");
     stxp ::llc::vcst_c  TNO_VERSION_BRANCH     = LLC_CXS("master");
-    stxp ::llc::vcst_c  TNO_VERSION_COMMIT     = LLC_CXS("362642be389e7a779a720ca5ac00b5fdc7943dd3");
-    stxp ::llc::vcst_c  TNO_VERSION_FILENAME   = LLC_CXS("tno.202506070420.bin");
+    stxp ::llc::vcst_c  TNO_VERSION_COMMIT     = LLC_CXS("e80bf5ad4a8e0716b9bfa6ad0b62300794a2f2d4");
+    stxp ::llc::vcst_c  TNO_VERSION_FILENAME   = LLC_CXS("tno.202506070601.bin");
     stxp ::llc::vcst_c  TNO_VERSION_FILESYSTEM = LLC_CXS("./assets");
 }
 
